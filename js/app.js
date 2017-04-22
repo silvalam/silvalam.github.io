f0b42c2f7ca16e5103017210a5b32abe4db3250a
sylvialam <s.silva.lam@gmail.com>
2017-04-22 00:22:19 -0700

    reworking parallax website

---
+++ js/app.js
@@ -0,0 +1,12 @@
+/* Nav */
+(function (){
+	var prev = 0;
+	var $window = $(window);
+	var nav = $('.scrollhide-nav');	
+
+	$window.on('scroll', function(){
+	var scrollTop = $window.scrollTop();
+	nav.toggleClass('hidden', scrollTop > prev);
+	prev = scrollTop;
+});
+})();
